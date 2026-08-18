9181b7f7ee51180fe93049ee634fd9b7c587c243
kafuruka leo <kafurukaleo66@gmail.com>
2026-08-18 14:02:23 +0200

    routes

---
+++ frontend/src/pages/events/EventCards.jsx
@@ -0,0 +1,26 @@
+import {CarendarDays, MapPin}  from "lucide react";
+
+import {Link} from "react-router-dom";
+
+const eventCards = ({event})  =>{
+    return(
+        <div className="event-Cards">
+            <h3>{event.title}</h3>
+            <p>
+                <CarendarDays size ={16}/>
+                { new date(event.event_date).tolocaleDateString()}
+            </p>
+
+            <p>
+             <MapPin size ={16}/>
+             {event.location || "No location"}
+                </p>
+
+                <link to = {`/events/${event.id}`}>
+                view Event 
+                </link>
+        </div>
+    );
+}
+
+export default eventCards;
